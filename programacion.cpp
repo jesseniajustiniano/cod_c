7826b62539e4ce0e10a8165747246d76124db765
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-15 21:37:33 -0400

    Add files via upload

---
+++ programacion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	char caracter;
+	cout<<"Ingrese una letra";
+	cin>>caracter;
+	//Buscar la funcion para convertir en minusculas un caracter
+	caracter=tolower(caracter)
+	switch(caracter)
+	{
+	case 'a': 
+	case 'e': 
+	case 'i': 
+	case 'o': 
+	case 'u': cout<<"Es vocal";
+	break;
+	default: cout<<"No es una vocal";
+	}
+	return 0;
+}
+
